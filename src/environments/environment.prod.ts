dab84ac3b9b61ddc8c7dedd8a7f8ca01755206d9
royking01 <royokunbor@gmail.com>
2021-04-14 12:40:42 +0000

    node modules

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyB18AsG9WeR05hEFCHZdpouAzoQAD_i8D0",
+    authDomain: "velocity-2f8f3.firebaseapp.com",
+    projectId: "velocity-2f8f3",
+    storageBucket: "velocity-2f8f3.appspot.com",
+    messagingSenderId: "333777205907",
+    appId: "1:333777205907:web:8cbbcea0c77de2b090ab66",
+    measurementId: "G-1TY89TTSMV"
+  }
+};
